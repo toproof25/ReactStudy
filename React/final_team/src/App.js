0fd613e38313fab93bfecbde56ba95251ef22c9f
Embeu <kmnlmn123@gmail.com>
2023-12-09 22:59:39 +0900

    Update App.js

--- React/final_team/src/App.js
+++ React/final_team/src/App.js
@@ -9,32 +9,6 @@ import AppViewClass from "./AppViewClass.js";
 import axios from "axios";
 import "./css/App.css";
 
-/*
-로그인 페이지 
-  - 로그인 기능 (아이디랑 비밀먼호 일치하면 로그인 성공)
-  - 회원가입 기능 (유저 정보를 받아서 추가하기)
-  -  + 추가기능 하고싶으면 제작
-
-메인 페이지
-  - 대충 구성만 잡고, 공지사항 제목들 보이기 (학교 메인 페이지에 있는 공지사항처럼?)
-
-공지사항 페이지 (이러닝 캠퍼스 -> 이용안내 -> 공지사항 페이지 느낌처럼)
-  - 공지사항 [번호, 제목, 작성자, 작성일]을 순서로 공지사항을 보이게 하기
-  - 공지사항을 작성할 수 있어야함 (추가 C)
-  - 각 공지사항 클릭 시 해당 공지사항 내용을 볼 수 있게 (읽기 R)
-  - 각 공지사항을 수정, 삭제할 수 있어야함 (수정, 삭제 UD)
-
-강의목록 페이지
-  - 각 로그인 유저에 강의 목록을 보여줌
-  - 각 강의에 대해서 추가, 수정 삭제 가능
-  - 각 강의 수업 목록을 보여줌
-  - 각 수업 목록에 대해서 추가, 수정, 삭제 가능
-
-마이 페이지
-  - 내 회원 정보 리스트
-  - 내 강의 목록 리스트
-*/
-
 export default function App() {
   const [login, setLogin] = useState(false) // 로그인 여부를 확인
   const [userId, setUserId] = useState(null) // 현재 로그인한 사람의 정보
@@ -71,7 +45,7 @@ export default function App() {
       <Main {...{ login, userId, name, setLogin, page, handleSetUserId }} />
 
       <footer>
-        <div>*김상옥 201904022 / 김윤서 202104025</div>
+        <div>(팀장)김상옥 201904022 / 김윤서 202104025</div>
         <div>이용재 202002451 / 이정인 202084025</div>
       </footer>
     </div>
